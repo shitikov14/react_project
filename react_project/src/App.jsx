6eee4adbe725b31b3f18f26d6e081118d4d8fd6d
Viacheslav <v.shitikov14@gmail.com>
2025-11-26 22:14:29 +0300

    components, tabs

--- react_project/src/App.jsx
+++ react_project/src/App.jsx
@@ -1,43 +1,13 @@
 import './App.css'
-// моковые данные
-import { restaurants } from './constants';
+import { Main } from './layouts/Layout';
+import { RestaurantList } from './components/restaurants/RestaurantList';
+import { restaurants } from "./constants/restaurants";
 
 function App() {
   return (
-    <div>
-      <h1>Рестораны</h1>
-      <ul className='restaurants__list'>
-        {restaurants.map((restaurant) => (
-          <li className='restaurants__list-block' key={restaurant.id}>
-            <h2>{restaurant.name}</h2>
-            <div className='menu__list'>
-              <h3>Меню</h3>
-              <ul className='menu__list-block'>
-                {restaurant.menu.map((item) => (
-                  <li key={item.id}>
-                    {item.name} - {item.price}$
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <div className='reviews__list'>
-              <h3>Отзывы</h3>
-              <ul className='reviews__list-block'>
-                {restaurant.reviews.map((item) => (
-                  <li key={item.id}>
-                    <div className='review__block'>
-                        <p className='review__block-name'>{item.user}:</p>
-                        <p className='review__block-text'>{item.text}</p>
-                        <p className='review__block-rating'>{item.rating}</p>
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Main>
+      <RestaurantList restaurants={restaurants} />
+    </Main>
   );
 }
 
---
+++ react_project/src/components/restaurants/Restaurant.jsx
@@ -0,0 +1,19 @@
+import { MenuList } from "../menu/MenuList";
+import { ReviewList } from "../reviews/ReviewList";
+export const Restaurant = ({ restaurant }) => {
+  return (
+    <li className="restaurants__list-block">
+      <h2>{restaurant.name}</h2>
+      {restaurant.menu?.length ? (
+        <MenuList menu={restaurant.menu} />
+      ) : (
+        <p>Меню отстутствует</p>
+      )}
+      {restaurant.reviews?.length ? (
+        <ReviewList reviews={restaurant.reviews} />
+      ) : (
+        <p>Отзывов пока нет</p>
+      )}
+    </li>
+  );
+};
